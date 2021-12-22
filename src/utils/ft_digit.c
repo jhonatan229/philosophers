e5dec264295f437a1fff8e2fbff480763f7453ee
jhonatan229 <jhonatan229@outlook.com>
2021-12-22 16:01:49 +0100

    fix ft_digit.c

--- src/utils/ft_digit.c
+++ src/utils/ft_digit.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 00:46:13 by coder             #+#    #+#             */
-/*   Updated: 2021/12/15 17:54:34 by coder            ###   ########.fr       */
+/*   Updated: 2021/12/22 15:52:27 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_isdigit(char *str)
 		return (1);
 	while (str[count])
 	{
-		if (str[count] < '0' && str[count] > '9')
+		if (str[count] < '0' || str[count] > '9')
 			return (1);
 		count++;
 	}
